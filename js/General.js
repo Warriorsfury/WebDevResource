e230884b95b4c35e02090ab1130120b687dff7c6
Warriorsfury <Warriorsfuryq@yahoo.com>
2016-08-10 14:01:16 -0400

    Jquery - Nav
    Finally got the gosh dang Jquery to work, and the Nav was changed a fair
    bit. More will be added. Nav changed to be fixed through scrolling.

--- js/General.js
+++ js/General.js
@@ -1,5 +1,48 @@
 $(document).ready(function() {
+    function playslider() {
+        $('.target').fadeIn(300).delay(1500).fadeOut(300, function() {
+          $(this).removeClass('rainbow');
+          $(this).text("use this HTML?");
+        });
+        $('.target').fadeIn(300).delay(1500).fadeOut(300, function() {
+          $(this).text("make this CSS?");
+        });
+        $('.target').fadeIn(300).delay(1500).fadeOut(300, function() {
+          $(this).text("code this in Jquery?");
+        });
+        $('.target').fadeIn(300).delay(1500).fadeOut(300, function() {
+          $(this).text("use Media Queries?");
+        });
+        $('.target').fadeIn(300).delay(1500).fadeOut(300, function() {
+          $(this).text("make this Responsive?");
+        });
+        $('.target').fadeIn(300).delay(1500).fadeOut(300, function() {
+          $(this).text("know what this does?");
+        });
+        $('.target').fadeIn(300).delay(1500).fadeOut(300, function() {
+          $(this).text("make that slide?");
+        });
+        $('.target').fadeIn(300).delay(1500).fadeOut(300, function() {
+          $(this).text("animate this element?");
+        });
+        $('.target').fadeIn(300).delay(1500).fadeOut(300, function() {
+          $(this).text("make this table responsive?");
+        });
+        $('.target').fadeIn(300).delay(1500).fadeOut(300, function() {
+          $(this).text("change this color?");
+        });
+        $('.target').fadeIn(300).delay(1500).fadeOut(300, function() {
+          $(this).text("change the class?");
+        });
+        $('.target').fadeIn(300).delay(1500).fadeOut(300, function() {
+          $(this).text("make a contact forum?");
+        });
+        $('.target').fadeIn(300).delay(1500).fadeOut(300, function() {
+          $(this).addClass('rainbow');
+          $(this).text("turn this into a rainbow?");
+        });
+        $('.target').fadeOut(300, playslider);
 
-  
-
+    }
+    playslider();
 });
